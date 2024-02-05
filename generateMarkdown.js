de884e1ca267f15d3d9bf7b5bc498e3b941ce7ca
Alex Balsillie <151325250+abalsillie@users.noreply.github.com>
2024-02-05 17:07:19 +1100

    Add files via upload

---
+++ generateMarkdown.js
@@ -0,0 +1,23 @@
+// Generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.project}
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Credits
+  ${data.credits}
+  ## License
+  ${data.license}
+  ## How to Contribute
+  ${data.contribute}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  For questions, please contact: ${data.email}
+  Review full github repo here: ${data.username};
+`};
+
+module.exports = generateMarkdown;
